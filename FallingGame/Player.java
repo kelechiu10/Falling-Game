f8812f75251beffeffc920331456e78a2b1645d7
AnglerfishGonads <34356748+AnglerfishGonads@users.noreply.github.com>
2018-03-22 16:20:41 -0700

    Update Player.java

--- FallingGame/Player.java
+++ FallingGame/Player.java
@@ -6,17 +6,29 @@ public class Player extends GameObject
         super(x, y, imageFile);
         GAME_WIDTH = gameWidth;
     }
+    /** @overrides image dimensions in game object */
+    protected void getImageDimensions()
+    {
+        myWidth = image.getWidth();
+        myHeight = image.getHeight();       
+    }
     public void move(int deltaX)
     {
         //find next position
         //check if it goes out of left side of screen
-        int nextX = Math.max(getX() + deltaX, 0);
+        int nextX = getX() + deltaX;
         //if it touches right side of screen
-        if (nextX + getWidth() > GAME_WIDTH)
+        if (nextX + myWidth > GAME_WIDTH)
         {
             //dont let it move further
-            nextX = GAME_WIDTH - getWidth(); 
+            nextX = 0;
+            //nextX = GAME_WIDTH - getWidth(); 
         }
+        else //if it touches left
+            if (nextX  < 0)
+            {
+                nextX = GAME_WIDTH - myWidth;
+            }
         
         //  Move the component
         setX(nextX);
